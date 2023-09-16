fba8c7ea7e1394c6cc5c744dd447b9fe7cc2c71f
Cameron French <cameron.french92@gmail.com>
2023-09-15 21:47:41 -0400

    added download resume functionality for resume button

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -6,7 +6,11 @@ export default function Resume() {
   return (
   <div className="w-full max-w-screen-2xl">
     <Card className="mt-8 h-fit p-10">
-      <Button>Download Resume</Button>
+      <Button>
+        <a href="./assets/docs/2023-Resume.pdf" download="2023-Resume.pdf">
+        Download Resume
+        </a>
+        </Button>
     </Card>
   </div>
   );
